d5324b73a4d45214445954d46433dbafd30b4292
Wendy Chang <wendy.changw@gmail.com>
2018-10-18 21:27:44 -0400

    updated 002

--- Javascript/002_addTwoNumbers.js
+++ Javascript/002_addTwoNumbers.js
@@ -1,35 +1,29 @@
 var addTwoNumbers = function(l1, l2) {
 
-  // Edge Cases
-  if (!l1) return l2;
-  else if (!l2) return l1;
+  // Edge cases
+  if (!l1) return l2
+  if (!l2) return l1
 
-  let ptr1 = l1;
-  let ptr2 = l2;
-  let sum = ptr1.val + ptr2.val;
+  const newHead = new ListNode(0)
+  let listRunner = newHead
+  let p1 = l1
+  let p2 = l2
 
-  const newHead = new ListNode(sum % 10);
-  let ptrNew = newHead;
-  ptr1 = ptr1.next;
-  ptr2 = ptr2.next;
-  let carry = sum >= 10 ? 1 : 0;
+  let carryover = 0
 
-  while (ptr1 || ptr2 || carry) {
-    let val1 = ptr1 ? ptr1.val : 0;
-    let val2 = ptr2 ? ptr2.val : 0;
-    sum = val1 + val2 + carry;
+  while (p1 || p2 || carryover) {
+    const val1 = p1 ? p1.val : 0
+    const val2 = p2 ? p2.val : 0
+    const sum = val1 + val2 + carryover
+    carryover = sum >= 10 ? 1 : 0
 
-    // Set up carry for next iteration
-    carry = sum >= 10 ? 1 : 0;
-    ptrNew.next = new ListNode(sum % 10);
-
-    ptrNew = ptrNew.next;
-    if (ptr1) ptr1 = ptr1.next;
-    if (ptr2) ptr2 = ptr2.next;
+    listRunner.next = new ListNode(sum % 10)
+    listRunner = listRunner.next
+    if (p1) p1 = p1.next
+    if (p2) p2 = p2.next
   }
-
-  return newHead;
-};
+  return newHead.next
+}
 
 /**
  * Definition for singly-linked list.
